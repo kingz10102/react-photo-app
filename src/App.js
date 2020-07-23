a64ab7855e4a76c1ca9f03ef6f633de2f8612d32
Kok Kok <REMY@Koks-MacBook-Pro.local>
2020-07-23 10:22:50 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="top_heading">
+        <h2>FlickGram</h2>
+      </div>
+      
+      {/*top heading of appliaction*/}
+
+
+
+      {/* Application posts */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Post.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Post() {
+    return (
+        <div>
+            {/*heading profile pic bubble & username */}
+
+            {/* photo image posted  */}
+
+            {/* username & caption under photo */}
+        </div>
+    )
+}
+
+export default Post
